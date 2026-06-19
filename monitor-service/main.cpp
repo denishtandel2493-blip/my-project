748ed5379753816143bc69167b9ab2c8e601148e
denishtandel2493-blip <denishtandel2493@gmail.com>
2026-06-19 12:03:57 +0530

    Implement basic HTTP server in main.cpp

---
+++ monitor-service/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(8082);
+
+    bind(s,(sockaddr*)&addr,sizeof(addr));
+    listen(s,5);
+
+    while(true) {
+
+        int c = accept(s,nullptr,nullptr);
+
+        std::string response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: application/json\r\n\r\n"
+        R"({
+            "PC1":"ONLINE",
+            "PC2":"OFFLINE"
+        })";
+
+        send(c,response.c_str(),response.size(),0);
+        close(c);
+    }
+}
